e2835913c53186838748832ee741cc86ac31e9f0
Levi Mann <lmann4@ksu.edu>
2017-03-30 14:07:46 -0500

    Updated the scrumtastic project

--- src/resource/project.js
+++ src/resource/project.js
@@ -1,10 +1,9 @@
 "use strict";
 
-/** @module projects
+/** @module project
  * A RESTful resource representing a software project
- * implementing the L-CRUD methods.
+ * implementing the CRUD methods.
  */
-
 module.exports = {
   list: list,
   create: create,
@@ -14,33 +13,39 @@ module.exports = {
 }
 
 /** @function list
-* Sends a list of all projects as a JSON array.
-*/
+ * Sends a list of all projects as a JSON array.
+ * @param {http.incomingRequest} req - the request object
+ * @param {http.serverResponse} res - the response object
+ * @param {sqlite3.Database} db - the database object
+ */
 function list(req, res, db) {
-  db.all("SELECT * FROM projects", [], function(err, projects) {
-    if (err) {
+  db.all("SELECT * FROM projects", [], function(err, projects){
+    if(err) {
       console.error(err);
       res.statusCode = 500;
-      res.end("Server Error");
+      res.end("Server Error")
     }
     res.setHeader("Content-Type", "text/json");
     res.end(JSON.stringify(projects));
-  })
+  });
 }
 
 /** @function create
-* Creates a new Project
-*/
+ * Creates a new project and adds it to the database.
+ * @param {http.incomingRequest} req - the request object
+ * @param {http.serverResponse} res - the response object
+ * @param {sqlite3.Database} db - the database object
+ */
 function create(req, res, db) {
-  var body = ""
+  var body = "";
 
-  req.on("error", function(err) {
+  req.on("error", function(err){
     console.error(err);
     res.statusCode = 500;
-    res.end("Server Error");
+    res.end("Server error");
   });
 
-  req.on("data", function(data) {
+  req.on("data", function(data){
     body += data;
   });
 
@@ -49,7 +54,7 @@ function create(req, res, db) {
     db.run("INSERT INTO projects (name, description, version, repository, license) VALUES (?,?,?,?,?)",
       [project.name, project.description, project.version, project.repository, project.license],
       function(err) {
-        if (err) {
+        if(err) {
           console.error(err);
           res.statusCode = 500;
           res.end("Could not insert project into database");
@@ -57,48 +62,54 @@ function create(req, res, db) {
         }
         res.statusCode = 200;
         res.end();
-
-    })
+      }
+    );
   });
 }
 
 /** @function read
-* Serves a specific Project as a JSON string
-*/
+ * Serves a specific project as a JSON string
+ * @param {http.incomingRequest} req - the request object
+ * @param {http.serverResponse} res - the response object
+ * @param {sqlite3.Database} db - the database object
+ */
 function read(req, res, db) {
   var id = req.params.id;
-  db.get("SELECT * FROM projects WHERE id=?", [id], function(err, project) {
-    if (err) {
+  db.get("SELECT * FROM projects WHERE id=?", [id], function(err, project){
+    if(err) {
       console.error(err);
-      res.statusCode = 500
-      res.end("Server Error");
+      res.statusCode = 500;
+      res.end("Server error");
       return;
     }
-    if (!project) {
+    if(!project) {
       res.statusCode = 404;
       res.end("Project not found");
       return;
     }
     res.setHeader("Content-Type", "text/json");
-    res.send(JSON.stringify(project));
+    res.end(JSON.stringify(project));
   });
 }
 
-/** @function update
-* Updates a specific record with the supplied values
-*/
+
+/** @update
+ * Updates a specific record with the supplied values
+ * @param {http.incomingRequest} req - the request object
+ * @param {http.serverResponse} res - the response object
+ * @param {sqlite3.Database} db - the database object
+ */
 function update(req, res, db) {
   var id = req.params.id;
+  var body = "";
 
-  var body = ""
-
-  req.on("error", function(err) {
+  req.on("error", function(err){
     console.error(err);
     res.statusCode = 500;
-    res.end("Server Error");
+    res.end("Server error");
   });
 
-  req.on("data", function(data) {
+  req.on("data", function(data){
     body += data;
   });
 
@@ -107,7 +118,7 @@ function update(req, res, db) {
     db.run("UPDATE projects SET name=?, description=?, version=?, repository=?, license=? WHERE id=?",
       [project.name, project.description, project.version, project.repository, project.license, id],
       function(err) {
-        if (err) {
+        if(err) {
           console.error(err);
           res.statusCode = 500;
           res.end("Could not update project in database");
@@ -115,24 +126,26 @@ function update(req, res, db) {
         }
         res.statusCode = 200;
         res.end();
-
-    })
+      }
+    );
   });
 }
 
-/** @function destroy
-* Deletes a specific record with the supplied values
-*/
+/** @destroy
+ * Removes the specified project from the database.
+ * @param {http.incomingRequest} req - the request object 
+ * @param {http.serverResponse} res - the response object
+ * @param {sqlite3.Database} db - the database object
+ */
 function destroy(req, res, db) {
   var id = req.params.id;
   db.run("DELETE FROM projects WHERE id=?", [id], function(err) {
-    if (err) {
+    if(err) {
       console.error(err);
       res.statusCode = 500;
-      res.end("Server Error");
-      return;
+      res.end("Server error");
     }
     res.statusCode = 200;
     res.end();
-  })
+  });
 }
